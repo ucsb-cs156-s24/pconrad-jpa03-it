07018cda1b8e8ba4e6b7445a6be6507de990e52b
Andrew Peng <andrew.z.peng@gmail.com>
2024-03-11 16:02:09 -0700

    ap - add back localhost to pom.xml

--- src/test/java/edu/ucsb/cs156/example/web/ITOauthPlaywrightTest.java
+++ src/test/java/edu/ucsb/cs156/example/web/ITOauthPlaywrightTest.java
@@ -30,7 +30,6 @@ import edu.ucsb.cs156.example.helpers.StringSource;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
 
-@EnableRuleMigrationSupport
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("integration")
@@ -75,17 +74,14 @@ class ITOauthPlaywrightTest {
                     .willReturn(temporaryRedirect("{{formData request.body 'form' urlDecode=true}}{{{form.redirectUri}}}?code={{{randomValue length=30 type='ALPHANUMERIC'}}}&state={{{form.state}}}")));
 
         wme.stubFor(post(urlPathEqualTo("/oauth/token"))
-                    .willReturn(okJson("{\"token_type\": \"Bearer\",\"access_token\":\"{{randomValue length=20 type='ALPHANUMERIC'}}\"}")));
+                    .willReturn(okJson("{\"access_token\":\"{{randomValue length=20 type='ALPHANUMERIC'}}\",\"token_type\": \"Bearer\",\"expires_in\":\"3600\",\"scope\":\"https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email openid\"}")));
 
         wme.stubFor(get(urlPathMatching("/userinfo"))
-                .willReturn(aResponse()
-                        .withStatus(200)
-                        .withHeader("Content-Type", "application/json")
-                        .withBody("{\"sub\":\"107126842018026740288\"" +
+                .willReturn(okJson("{\"sub\":\"107126842018026740288\"" +
                                 ",\"name\":\"Andrew Peng\"" +
                                 ",\"given_name\":\"Andrew\"" +
                                 ",\"family_name\":\"Peng\"" +
-                                ", \"picture\":\"https://lh3.googleusercontent.com/a/ACg8ocJpOe2SqIpirdIMx7KTj1W4OQ45t6FwpUo40K2V2JON=s96-c\"" +
+                                ",\"picture\":\"https://lh3.googleusercontent.com/a/ACg8ocJpOe2SqIpirdIMx7KTj1W4OQ45t6FwpUo40K2V2JON=s96-c\"" +
                                 ", \"email\":\"andrewpeng@ucsb.edu\"" +
                                 ",\"email_verified\":true" +
                                 ",\"locale\":\"en\"" +
@@ -110,18 +106,13 @@ class ITOauthPlaywrightTest {
     }
 
     @Test
-    public void tryLogin() throws Exception {
+    public void tryLogin() throws Exception { 
         String url = String.format("http://localhost:%d/oauth2/authorization/my-oauth-provider", port);
         page.navigate(url);
-        page.locator("#username").fill("andrewpeng@ucsb.edu");
-        page.locator("#password").fill("password");
+
+        page.fill("input[name=username]", "andrewpeng@ucsb.edu");
+        page.fill("input[name=password]", "password");
         page.locator("#submit").click();
-
-        // String cURL = page.url();
-        // assertSame("http://localhost:8080/", cURL);
-
-        // String cURL = String.format("http://localhost:%d/profile", port);
-        // page.navigate(cURL);
 
         String bodyHTML = page.innerHTML("body");
         String expectedHTML = StringSource.getIntegrationDefaultLocalhostContent();
